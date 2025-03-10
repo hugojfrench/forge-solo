4e1245edc7b929b1e5e881f20d172233e09daedd
Hugo French <hugojfrench@icloud.com>
2025-03-10 17:46:54 +0000

    tag model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_10_174533) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_10_174635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_10_174533) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["idea_id"], name: "index_posts_on_idea_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

b41daba0ef248737715be89018c7da903a949ef0
Tony Udotong <tonyudotong@gmail.com>
2025-03-11 15:26:12 +0000

    create action logic done

--- app/controllers/initial_questions_controller.rb
+++ app/controllers/initial_questions_controller.rb
@@ -7,11 +7,20 @@ class InitialQuestionsController < ApplicationController
   end
 
   def create
+    @initial_questions = current_user.initial_questions # needed in case of validation error
+    text = "I am a #{initial_question_params[:i_am_a]}. I am interested in #{initial_question_params[:i_am_interested_in]}. I am seeking inspiration for #{initial_question_params[:seeking_inspiration_for]}. I am interested in the #{initial_question_params[:ideation_framework]} ideation framework. Additional info: #{initial_question_params[:additional_info]}"
+    @initial_question = InitialQuestion.new(user_question: text)
+    @initial_question.user = current_user
+    if @initial_question.save
+      redirect_to "/initial_questions/#{@initial_question.id}"  #redirect to the initial questions show page
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def initial_question_params
-    params.require(:initial_question).permit(:i_am_a, :ideation_framework, :i_am_interested_in, :seeking_inspiration_for, :additional_info)
+    params.permit(:i_am_a, :ideation_framework, :i_am_interested_in, :seeking_inspiration_for, :additional_info)
   end
 end

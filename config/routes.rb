164c06e3a22213f27352f5ceed102d8cdfae8cc7
Hugo French <hugojfrench@icloud.com>
2025-03-11 17:14:08 +0000

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   # Defines the route for the posts index and show
-  resourcs :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show]
 
   # Define route for the UI Kit page
   get "ui_kit", to: "pages#ui_kit", as: :ui_kit

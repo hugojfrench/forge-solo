36780c719a0e5764578ccb43a207965d298727c2
Hugo French <hugojfrench@icloud.com>
2025-03-11 08:56:03 +0000

    has manys added to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :idea
+  has_many :feedbacks, dependent: :destroy
+  has_many :post_sections, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 end

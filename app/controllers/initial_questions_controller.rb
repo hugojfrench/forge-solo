8c8fd5ebdfdf49dcc172cc9ba751ea933c7a3534
Tony Udotong <tonyudotong@gmail.com>
2025-03-11 14:42:43 +0000

    params added

--- app/controllers/initial_questions_controller.rb
+++ app/controllers/initial_questions_controller.rb
@@ -7,6 +7,12 @@ class InitialQuestionsController < ApplicationController
   end
 
   def create
+    raise
   end
 
+  private
+
+  def initial_question_params
+    params.require(:initial_question).permit(:i_am_a, :ideation_framework, :i_am_interested_in, :seeking_inspiration_for, :additional_info)
+  end
 end

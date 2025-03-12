66b50902edf02a9126a78eb5955e1172a2ae2096
Hugo French <hugojfrench@icloud.com>
2025-03-12 14:43:56 +0000

    added turbo stream to feedback form

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -5,9 +5,16 @@ class FeedbacksController < ApplicationController
     @feedback.post = @post
     @feedback.user = current_user
     if @feedback.save
-      redirect_to @post
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(:feedbacks, partial: "feedbacks/feedback",
+            target: "feedbacks",
+            locals: { feedback: @feedback })
+        end
+        format.html { redirect_to post_path(@post) }
+      end
     else
-      raise
+      render "posts/show", status: :unprocessable_entity
     end
   end
 

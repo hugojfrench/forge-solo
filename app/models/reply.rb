564724f8a8d57abc62b3e904ffa054fc9feeccf0
Hugo French <hugojfrench@icloud.com>
2025-03-12 16:51:55 +0000

    turbo live, appends in wrong place!

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,14 @@
 class Reply < ApplicationRecord
   belongs_to :user
   belongs_to :feedback
+  after_create_commit :broadcast_reply
+
+  private
+
+  def broadcast_reply
+    broadcast_append_to "feedback_#{feedback.id}_replies",
+                        partial: "replies/reply",
+                        target: "replies",
+                        locals: { reply: self }
+  end
 end

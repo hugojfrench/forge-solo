ddd0b567323860bf82c74cda469d97ad033a9b54
Milenco Ieremici <milenco@widgit.com>
2025-03-12 17:59:19 +0000

    Changed the form to work better with nested resources and update the values when pressing save

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,9 +1,24 @@
 class IdeasController < ApplicationController
+  before_action :set_idea, only: %i[edit update]
+
   def index
     @ideas = current_user.ideas
   end
 
   def edit
+  end
+
+  def update
+    @idea.update(idea_params)
+  end
+
+  private
+
+  def set_idea
     @idea = Idea.find_by_id(params[:id])
+  end
+
+  def idea_params
+    params.require(:idea).permit(:title, :tagline, :summary, idea_sections_attributes: %i[id heading content])
   end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,7 @@ class Idea < ApplicationRecord
   belongs_to :user
   has_many :posts, dependent: :destroy
   has_many :idea_sections, dependent: :destroy
+  accepts_nested_attributes_for :idea_sections
 
   def upvotes
     # the total upvotes of the idea are equal with the upvotes of the posts associated

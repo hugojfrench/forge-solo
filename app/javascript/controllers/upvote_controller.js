5733b43b6abf114c5b5a4d8da2d0618bff95f5d6
Hugo French <hugojfrench@icloud.com>
2025-03-13 21:53:13 +0000

    stimulus controller implemented and working!

--- app/javascript/controllers/upvote_controller.js
+++ app/javascript/controllers/upvote_controller.js
@@ -2,12 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="upvote"
 export default class extends Controller {
+  static targets = ["count"]
+  static values = { url: String }
+
   connect() {
     console.log("Upvote controller connected!");
   }
 
   upvote(event) {
     event.preventDefault();
-    console.log("Upvote button clicked!");
+    // console.log("Upvoting...");
+
+    fetch(this.urlValue, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content,
+        "Accept": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) { throw new Error("Network response was not ok"); }
+        return response.json();
+      })
+      .then(data => {
+        this.countTarget.innerText = data.upvotes;
+      })
+      .catch(error => {
+        console.error("Upvote failed:", error);
+      });
   }
 }

06173e6b3acfd123fd6d708c571e733efaf388b4
Hugo French <hugojfrench@icloud.com>
2025-03-13 21:25:46 +0000

    upvote button action connected

--- app/javascript/controllers/upvote_controller.js
+++ app/javascript/controllers/upvote_controller.js
@@ -3,6 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="upvote"
 export default class extends Controller {
   connect() {
-    console.log("✅ Upvote controller connected!");
+    console.log("Upvote controller connected!");
+  }
+
+  upvote(event) {
+    event.preventDefault();
+    console.log("Upvote button clicked!");
   }
 }

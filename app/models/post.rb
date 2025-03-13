1deea3c0d9734006541863b4b22a8eef09c33e9f
Hugo French <hugojfrench@icloud.com>
2025-03-13 22:33:08 +0000

    basic sort on feedbacks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :idea
-  has_many :feedbacks, dependent: :destroy
+  has_many :feedbacks, -> { order(upvotes: :desc) }, dependent: :destroy
   has_many :post_sections, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags

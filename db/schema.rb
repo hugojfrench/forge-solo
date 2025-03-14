cc9a54fe686f64b4d2b4721db539361c10b6c807
Milenco Ieremici <milenco@widgit.com>
2025-03-14 16:10:40 +0000

    Remove '#index' from Posts view page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.2].define(version: 2025_03_14_120219) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,7 +89,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_14_120219) do
     t.text "ai_answer1"
     t.text "ai_answer2"
     t.text "ai_answer3"
-
     t.string "title1"
     t.string "title2"
     t.string "title3"
@@ -101,7 +98,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_14_120219) do
     t.text "summary1"
     t.text "summary2"
     t.text "summary3"
-
     t.index ["user_id"], name: "index_initial_questions_on_user_id"
   end
 

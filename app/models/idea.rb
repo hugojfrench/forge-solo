b9257fe48c148399a5f81198c2c5c5e61d42c259
Tony Udotong <tonyudotong@gmail.com>
2025-03-14 15:04:41 +0000

    fucking done

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,6 +4,7 @@ class Idea < ApplicationRecord
   has_many :idea_sections, dependent: :destroy
   accepts_nested_attributes_for :idea_sections
   after_create :set_content
+  after_create :destroy_initial_questions
 
   def upvotes
     # the total upvotes of the idea are equal with the upvotes of the posts associated
@@ -21,10 +22,7 @@ class Idea < ApplicationRecord
       messages: [{ role: "system", content: "You are an assistant for an idea generator platform. Take this idea and expand it into 3 key sections. These sections should allow the user to take their idea and run with it. For each section, output 4 sentences. Seperate each element by ###. Both the section header and content should be seperated by ###. The format should look like, 'Section1 header ### Section1 content ### Section2 header ### Section 2 content ### Section 3 header ### Section 3 content'. Make sure your output follows exactly this structure. Do not include any extra words or phrases." }, { role: "user", content: self.title  + self.tagline + self.summary }]
     })
     new_content = initial_chatgpt_response["choices"][0]["message"]["content"]
-    # p new_content
     array_new_content = new_content.split("###").reject { |c| c == "" }  #split the new content into an array and remove empty strings
-    # puts "here is the array"
-    # p array_new_content
     array_new_content
     add_sections(array_new_content)
   end
@@ -33,14 +31,9 @@ class Idea < ApplicationRecord
      IdeaSection.create(heading: array[0], content: array[1], idea_id: self.id)
      IdeaSection.create(heading: array[2], content: array[3], idea_id: self.id)
      IdeaSection.create(heading: array[4], content: array[5], idea_id: self.id)
-     puts "here are the sections"
-     p self.idea_sections
+  end
+
+  def destroy_initial_questions      #destroys all previous initial questions
+    user.initial_questions.destroy_all
   end
 end
-# initial idea is broken up into title, tagline, summmary - done
-# the initial idea is being sent to the AI with a prompt to "expand" - done
-# AI returns the expanded idea seperated into sections - returns the idea with 3 sections but each section is not its own idea_section instance yet. Might be worth adding columns to the ideas table for the section headers and content.
-# idea_sections are created and associated with the idea
-# both are saved to the database
-# user is brought to idea#edit page - yes but the sections are not displayed yet
-# destroy all of the previous initial questions

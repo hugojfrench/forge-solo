b942ea5bfadacfa2d72acf29a413c7fd65f4a173
Hugo French <hugojfrench@icloud.com>
2025-03-14 14:19:37 +0000

    added submit image on post new (optional) and if exists show image on post show

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   accepts_nested_attributes_for :post_sections, allow_destroy: true
+  has_one_attached :image
 end

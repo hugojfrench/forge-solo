39479b2dd0f3d9b25bf8783534578e1f8bfd4636
Hugo French <hugojfrench@icloud.com>
2025-03-17 15:17:55 +0000

    toggle hidden working

--- app/javascript/controllers/toggle_replies_controller.js
+++ app/javascript/controllers/toggle_replies_controller.js
@@ -2,9 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-replies"
 export default class extends Controller {
+  static targets = ["replies"]
+
   connect() {
     console.log("ToggleReplies controller connected");
   }
 
-  
+  toggle() {
+    this.repliesTarget.classList.toggle("hidden");
 }

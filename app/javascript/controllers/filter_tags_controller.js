1361cf2e0e1e660ec39e8f6355be625241dd3d20
Milenco Ieremici <milenco@widgit.com>
2025-03-18 16:30:04 +0000

    Remove a tag filter

--- app/javascript/controllers/filter_tags_controller.js
+++ app/javascript/controllers/filter_tags_controller.js
@@ -33,11 +33,29 @@ export default class extends Controller {
   }
 
   insertTag(tag) {
-    const newTag = `<div class='tag small'>${tag.innerHTML}</div>`
+    const newTag = `<div class='tag small'>
+      ${tag.innerHTML}
+      <span data-action='click->filter-tags#removeItem'><i class="fa-solid fa-x"></i></span>
+      </div>`
     this.currentTagsTarget.insertAdjacentHTML("beforeend", newTag);
   }
 
   submitForm() {
     this.formTarget.requestSubmit();
+  }
+
+  removeItem(event) {
+    const domToBeRemoved = event.target.parentElement.parentElement;
+    const tagToBeRemoved = domToBeRemoved.innerText.trim();
+
+    let itemValueToBeRemoved = 0;
+    Object.keys(this.selectTags.options).forEach( value => {
+      if (this.selectTags.options[value]['text'] === tagToBeRemoved) {
+        itemValueToBeRemoved = value;
+      }
+    });
+
+    this.selectTags.removeItem(itemValueToBeRemoved, false);
+    domToBeRemoved.remove();
   }
 }

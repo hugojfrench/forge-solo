809af2a9472b5f81b7ddf8296c1e34815acf71f2
Louise Stone <louisemstone@hotmail.com>
2025-03-21 08:13:26 +0000

    adding old code back in but commented so that it's easy to revert

--- app/javascript/controllers/initial_questions_sidebar_controller.js
+++ app/javascript/controllers/initial_questions_sidebar_controller.js
@@ -21,6 +21,17 @@ export default class extends Controller {
   }
 }
 
+// OLD METHOD PRE STYLE FIXES
+// toggleSidebar() {
+//   this.sidebarTarget.classList.toggle("hidden");
+//   this.flexboxTarget.classList.toggle("flexwrap");
+//   this.cardTargets.forEach((card) => {
+//     card.classList.toggle("flip-card-row");
+//     card.classList.toggle("flip-card-column");
+//   });
+//   this.titleTarget.classList.toggle("title");
+// }
+
 // refersh button - clicked should open sidebar
 // x in side bar should close sidebar
 // clicking outside sidebar should close sidebar
